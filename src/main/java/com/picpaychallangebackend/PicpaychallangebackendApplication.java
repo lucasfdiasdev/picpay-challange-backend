861c70d97e76077ea308d3f51f7c016c8e2d78a4
Lucas Dias <lucasfdiasdev@gmail.com>
2024-03-17 18:43:53 -0300

    feat: initialize commit

---
+++ src/main/java/com/picpaychallangebackend/PicpaychallangebackendApplication.java
@@ -0,0 +1,13 @@
+package com.picpaychallangebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpaychallangebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpaychallangebackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/picpaychallangebackend/PicpaychallangebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picpaychallangebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpaychallangebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
