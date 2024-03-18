db3c62f8811fd3b0f13035ed52823b3114336682
Lucas Dias <lucasfdiasdev@gmail.com>
2024-03-18 00:09:52 -0300

    feat: create UserDTO

---
+++ src/main/java/com/picpaychallangebackend/dtos/UserDTO.java
@@ -0,0 +1,8 @@
+package com.picpaychallangebackend.dtos;
+
+import com.picpaychallangebackend.domain.user.UserType;
+
+import java.math.BigDecimal;
+
+public record UserDTO(String firstName, String lastName, String document, BigDecimal balance, String email, String password, UserType userType) {
+}
